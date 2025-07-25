d7c5d05e7bcdf88d067c43d852185e26185bcf93
Sanket Panchal <Sanketpanchal775@gmail.com>
2025-07-25 18:44:37 +0530

    fix: Add missing commas and sameSite attribute to cookie options

--- Backend/src/Constants/cookieOptions.js
+++ Backend/src/Constants/cookieOptions.js
@@ -1,13 +1,15 @@
 const accessTokenOptions = {
     httpOnly : true,
     secure : true,
-    maxAge : 10 * 60 * 1000
+    maxAge : 10 * 60 * 1000,
+    sameSite: 'None',
 }
 
 const refreshTokenOptions = {
     httpOnly : true,
     secure : true,
-    maxAge: 10 * 60 * 1000 * 60 * 24
+    maxAge: 10 * 60 * 1000 * 60 * 24,
+    sameSite: 'None',
 }
 
 export { 
