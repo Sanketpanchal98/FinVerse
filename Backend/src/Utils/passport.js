0ba902e8bd960ace3b989dfcc2aa57cc435cb4d2
Sanket Panchal <Sanketpanchal775@gmail.com>
2025-07-25 17:42:58 +0530

    Added passport callback-url

--- Backend/src/Utils/passport.js
+++ Backend/src/Utils/passport.js
@@ -6,7 +6,7 @@ import { User } from '../Models/User.model.js';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/api/v1/auth/google/callback"
+    callbackURL: "https://finverse-backend-roan.vercel.app/api/v1/auth/google/callback"
 }, async (AccessToken, RefreshToken, profile, done) => {
     try {
         
